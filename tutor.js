be5f147c46cef1e3f3bb07b003e091900e48149c
vo777 <76571140+vo777@users.noreply.github.com>
2021-05-27 04:52:06 -0700

    Update tutor.js

--- tutor.js
+++ tutor.js
@@ -206,8 +206,15 @@ function newQuestion()
 	
 	const N = data.dict.length;
 	
+const oldQ = data.dict[data.currentIndex][0];
+
+for (let i=0: i<100; ++i)
+{
+
 	data.currentIndex = Math.floor(data.windowStart + Math.random()*WindowSize) % N;
-	
+	if (data.dict[data.currentIndex][0] != oldQ) break;
+}
+
 	console.log(data.currentIndex, ':', data.dict[data.currentIndex]);
 	
 	data.answerIndices 
