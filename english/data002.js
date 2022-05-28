175c6ae655c2d1faf776d976fcf4f027a5ee50be
vo777 <76571140+vo777@users.noreply.github.com>
2022-05-28 14:04:49 -0400

    Add files via upload

---
+++ english/data002.js
@@ -0,0 +1,104 @@
+const data002 = [
+`
+I
+we
+/
+look at
+talk about
+call
+help
+looked at
+talked about
+called
+helped
+will look at
+will talk about
+will call
+will help
+/
+him
+her
+them
+/
+every day
+often
+`,
+`
+I
+we
+/
+do not call
+do not help
+/
+him
+her
+them
+/
+any more
+at all
+ever
+`,
+`
+I
+we
+/
+forgot
+/
+him
+her
+`,
+`
+When
+Why
+How ofter
+/
+did
+will
+/
+you
+he
+they
+/
+go to
+came from
+/
+New York
+Ukraine
+/
+?
+`
+,
+`
+Who
+/
+did
+/
+you
+I
+they
+he
+/
+call
+see
+meet
+/
+last week
+often
+two months ago
+/
+?
+`,
+`
+do you
+does he
+/
+live
+work
+party
+/
+in New York
+in France
+/
+?
+`
+];
