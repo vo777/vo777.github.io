9b23654b1699c4ebc5dcdcd0bd604f87329a499d
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-01 13:21:54 -0400

    Add files via upload

--- english/data005.js
+++ english/data005.js
@@ -68,103 +68,190 @@ he is the oldest
 brother
 of the children
 ======================
+She is the most beautiful in this class
+=======================
 
-
-`;
-
-
-
-
-`
-more beautiful
-the most beautiful
-
-most likely
-
-good , better than , the best
-bad  , worse  than , the worst
-
-=======
-
-yesterday
-today
-tomorrow
-now
-
+I will
+I'll
+/
+call
+help
+forget
+see
+/
+you
+/
 in 3 days
 in 2 weeks
+tomorrow
+next week
+in one week
+in a week
+
+===============
+
+I'll be back
+I will come
+I will come back
+/
 in 4 months
 in 5 years
 
+=========
+I
+/
+was here
+saw him
+/
 3 days ago
 2 weeks ago
 4 months ago
 5 years ago
+=============
 
-I will come in ...
-I will see you in a week
+I will be 
+I'll be
+/
+in office
+at home
+back
+there
+here
+/
+today
+tomorrow
+next week
+in a week
+in one week
+in 2 weeks
+=================
 
+I
+He
+/
+will go to
+will be
+/
+Norway
+Paris
+France
+/
+in
+next
+this
+/
+winter
+spring
+summer
+autumn (fall)
+January
+February
+March
+April
+May
+June
+July
+August
+September
+October
+November
+December
+====================
 
-He went ... ago
-
-I saw him 2 weeks ago
-
-I will be in office / at home / back / there / here
-
-I'll be ....
-
-I will go to xxx in winter
-We will go home in 1 hour  / in an hour
-
-I was here yesterday
-
-I will be here tomorrow
-
-at 5 pm -- --
-
-on Monday -- --
-on Sunday
-
-in May
-in winter
-
-next winter
-
-before after
-
-spring comes after winter
-
-on / Monday ... Sunday
-
-I will come on Monday
-
-in / January ... December
-
-bless you
-
-be well
+I
+He
+/
+went to
+was in
+/
+Norway
+Paris
+France
+/
+in
+last
+this
+/
+winter
+spring
+summer
+autumn (fall)
+January
+February
+March
+April
+May
+June
+July
+August
+September
+October
+November
+December
+====================
 
 =====
 
+I'll see you
+/
 this
 next
+/
+week
+month
+year
+
+=====
+I'll see you
+/
+at 
+/
+9 am
+5 pm
+=======
+
+I'll see you
+/
+in the 
+/
+Morning
+Evening
+=======
+
+I saw you
+/
+this
 last
 /
 week
 month
 year
-time
-
-=======
-
-winter
-spring
-summer
-autumn(fall)
-
-=======
+===========
 
 
+I'll see you
+I saw you
+/
+on
+/
+Monday
+Tuesday
+Wednesday
+Thursday
+Friday
+Saturday
+Sunday
 
-`
-;
+==========
+spring comes after winter
+winter comes before spring
+==========
+
+I will
+/
+most likely
+/
+work tomorrow
+
+`;
+
