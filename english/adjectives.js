cae2f8434f98159c908f9fca71884dde9026bb77
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-01 15:32:14 -0400

    Add files via upload

---
+++ english/adjectives.js
@@ -0,0 +1,50 @@
+const adj = `GOOD 	ХОРОШИЙ	 BAD 	ПЛОХО
+BIG 	БОЛЬШОЙ	 SMALL 	 МАЛЕНЬКИЙ
+OLD 	СТАРЫЙ	 NEW; YOUNG 	 НОВЫЙ; МОЛОДОЙ
+FAST 	БЫСТРО	 SLOW 	 МЕДЛЕННЫЙ
+HAPPY 	СЧАСТЛИВЫЙ	 SAD 	 ГРУСТНЫЙ
+RIGHT 	ПРАВИЛЬНО	 WRONG 	 НЕПРАВИЛЬНЫЙ
+STRONG 	СИЛЬНЫЙ	 WEAK 	 СЛАБЫЙ
+CHEAP 	ДЕШЕВЫЙ	 EXPENSIVE 	 ДОРОГОЙ
+BEAUTIFUL 	КРАСИВАЯ	 UGLY 	 УРОДЛИВЫЙ
+NEAR	ОКОЛО	 CLOSE 	 ЗАКРЫТЬ
+DIFFERENT 	ДРУГОЙ	 SAME 	 ТАКОЙ ЖЕ
+INTERESTING; EXCITING 	ИНТЕРЕСНО; ЗАХВАТЫВАЮЩЕ	 BORING 	 СКУЧНЫЙ
+COOL; AWESOME 	КРУТО; ПОТРЯСАЮЩИЕ	 LAME 	ХРОМОЙ
+EASY 	ЛЕГКИЙ	 DIFFICULT 	 СЛОЖНО
+FREE 	СВОБОДНО	 BUSY 	 ЗАНЯТЫЙ
+SHORT 	КОРОТКАЯ	LONG	ДЛИННАЯ
+HIGH 	ВЫСОКАЯ	 LOW 	 НИЗКИЙ
+COLD 	ХОЛОДНО	 WARM; HOT 	 ТЕПЛО; ГОРЯЧИЙ
+CLEAN 	ЧИСТЫЙ	 DIRTY 	 ГРЯЗНЫЙ
+SAFE 	БЕЗОПАСНО	 DANGEROUS 	 ОПАСНЫЙ
+GREAT 	БОЛЬШОЙ	 AWFUL 	 УЖАСНЫЙ
+POSITIVE 	ПОЛОЖИТЕЛЬНО	 NEGATIVE 	 ОТРИЦАТЕЛЬНО
+NORMAL 	ОБЫЧНЫЙ	 STRANGE 	 СТРАННЫЙ
+RICH 	БОГАТЫЙ	 POOR 	БЕДНЫЙ
+EARLY 	РАНО	 LATE 	 ПОЗДНО
+USEFUL 	ПОЛЕЗНЫЙ	 USELESS 	 БЕСПОЛЕЗНЫЙ
+CALM 	СПОКОЙСТВИЕ	 ANGRY; NERVOUS 	 ЗЛОЙ; НЕРВНЫЙ
+SMART 	УМНАЯ	 STUPID 	 ГЛУПЫЙ
+COMFORTABLE 	КОМФОРТНЫЙ	 UNCOMFORTABLE 	 НЕУДОБНЫЙ
+OPEN 	ОТКРЫТЫМ	 CLOSED 	ЗАКРЫТО
+HEALTHY 	ЗДОРОВЫЙ	 SICK 	 БОЛЬНОЙ
+FULL 	ПОЛНЫЙ	 EMPTY; HUNGRY 	 ПУСТОЙ; ГОЛОДНЫЙ
+LIGHT 	ЛЕГКИЙ		
+SUCCESSFUL 	УСПЕШНЫЙ	 UNSUCCESSFUL 	 НЕУСПЕШНО
+IMPORTANT 	ВАЖНЫЙ	 UNIMPORTANT 	 НЕВАЖНО
+REAL 	НАСТОЯЩИЙ	 FAKE 	ФАЛЬШИВЫЙ
+SOFT 	МЯГКИЙ	 HARD 	 ЖЕСТКИЙ
+QUIET 	ТИХО	 LOUD	 ГРОМКИЙ
+POSSIBLE 	ВОЗМОЖНЫЙ	 IMPOSSIBLE 	 НЕВОЗМОЖНО
+THIN 	ТОНКИЙ	NOISY	ШУМНЫЙ
+COMMON 	ОБЩИЙ	 UNIQUE 	 УНИКАЛЬНЫЙ
+FAIR 	СПРАВЕДЛИВЫЙ	 UNFAIR 	НЕСПРАВЕДЛИВЫЙ
+ABLE 	СПОСОБНЫЙ	 UNABLE 	 НЕСПОСОБНЫЙ
+USUAL 	ОБЫЧНЫЙ	 UNUSUAL 	 НЕОБЫЧНЫЙ
+FRIENDLY 	ДРУЖЕЛЮБНЫЙ	 AGGRESSIVE 	 АГРЕССИВНЫЙ
+PLEASANT 	ПРИЯТНЫЙ	 UNPLEASANT 	 НЕПРИЯТНО
+GENERAL 	ГЕНЕРАЛЬНАЯ	 SPECIFIC 	 СПЕЦИФИЧЕСКИЙ
+PRIVATE 	ЧАСТНЫЙ	 PUBLIC 	ОБЩЕСТВЕННЫЙ
+FRESH 	СВЕЖИЙ	 TIRED 	 УСТАЛА
+DEAD 	МЕРТВЫХ	 ALIVE 	 В ЖИВЫХ`;
