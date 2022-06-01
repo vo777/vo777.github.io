df8682a0c557f26f3c7829202d753d782f306b4b
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-01 13:00:06 -0400

    Add files via upload

---
+++ english/data005.js
@@ -0,0 +1,170 @@
+const data005 =     `
+
+He
+/
+is
+/
+young
+old
+beautiful
+ugly
+smart
+fat
+
+older than her
+smarter than her
+more beautiful than her
+a better student than her
+
+the oldest
+the youngest
+the best student
+the smartest
+
+====
+
+today I speak better than
+/
+yesterday
+three days ago
+a month ago
+one month ago
+last week
+last month
+=====
+yesterday 
+three days ago
+a month ago
+one month ago
+last week
+last month
+/
+I spoke worse than
+/
+today
+now
+===============
+today she 
+/
+is more beautiful
+came later
+is better prepared
+came better prepared
+/
+than yesterday
+================================
+November is shorter than October
+February is the shortest month
+I am the best ..profession..
+I like January more than February
+====================================
+he is older than 
+/
+you
+his brother
+======================
+he is the oldest
+/
+brother
+of the children
+======================
+
+
+`;
+
+
+
+
+`
+more beautiful
+the most beautiful
+
+most likely
+
+good , better than , the best
+bad  , worse  than , the worst
+
+=======
+
+yesterday
+today
+tomorrow
+now
+
+in 3 days
+in 2 weeks
+in 4 months
+in 5 years
+
+3 days ago
+2 weeks ago
+4 months ago
+5 years ago
+
+I will come in ...
+I will see you in a week
+
+
+He went ... ago
+
+I saw him 2 weeks ago
+
+I will be in office / at home / back / there / here
+
+I'll be ....
+
+I will go to xxx in winter
+We will go home in 1 hour  / in an hour
+
+I was here yesterday
+
+I will be here tomorrow
+
+at 5 pm -- --
+
+on Monday -- --
+on Sunday
+
+in May
+in winter
+
+next winter
+
+before after
+
+spring comes after winter
+
+on / Monday ... Sunday
+
+I will come on Monday
+
+in / January ... December
+
+bless you
+
+be well
+
+=====
+
+this
+next
+last
+/
+week
+month
+year
+time
+
+=======
+
+winter
+spring
+summer
+autumn(fall)
+
+=======
+
+
+
+`
+;
