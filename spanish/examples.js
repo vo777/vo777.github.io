e8c0ca8bccd97214123703f11a61ae332cbe5f86
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-05 10:02:10 -0400

    Add files via upload

--- spanish/examples.js
+++ spanish/examples.js
@@ -16,6 +16,9 @@ no tengo algo
 no tengo algo importante
 no tienes
 no tienes algo
+
+es normal
+no es normal
 /
 para mi
 para ti
@@ -32,6 +35,301 @@ no tengo mucho dinero
 todavia me encuentro mal
 es peligroso
 =====
+
+qué
+a qué hora
+/
+quieres
+puedes
+vas a
+/
+estudiar
+comer
+hacer
+venir
+/
+hoy
+mañana
+=================
+
+cuando
+/
+te
+lo
+/
+ayudamos
+llamamos
+besamos
+compramos
+vendemos
+cojemos
+arreglamos
+================
+te
+lo
+/
+ayudamos
+llamamos
+besamos
+compramos
+vendemos
+cojemos
+arreglamos
+/
+mañana
+ahora
+=============
+cuando
+/
+te
+lo
+/
+ayudo
+llamo
+beso
+compro
+vendo
+cojo
+arreglo
+=================
+te
+lo
+/
+ayudo
+llamo
+beso
+compro
+vendo
+cojo
+arreglo
+/
+mañana
+ahora
+============
+qué
+/
+pasa
+quieres
+dices
+buscas
+/
+ahora
+==========
+
+es
+necesito
+tengo
+/
+posible
+imposible
+algo
+comer
+frio
+/
+ahora
+para ti
+asi
+=====
+
+quiero
+necesito
+voy a
+es
+/
+comer
+comprar
+comprarle
+mostrarle
+hacerle
+hacerlo
+lo que
+/
+algo
+ahora
+necesito
+quiero
+========
+
+si
+/
+compro
+hago
+comprara
+/
+algo
+eso
+=========
+donde
+/
+esta
+estan
+es
+quedamos
+/
+la estacion
+Maria
+los servicios
+tus amigos
+la reunion
+================
+quanto
+quantos
+/
+cuesta
+tiempo
+tienes
+/
+la entrada
+esto
+hay
+======
+cuando
+a que hora
+/
+llega
+sale
+es
+quedamos
+/
+el tren
+el autobus
+la reunion
+============
+le compre
+le mostre
+comi
+hice
+pude
+compraron
+/
+algo
+lo que
+para ti
+por ti
+hacerlo
+/
+ayer
+para ti
+=============
+yo estoy
+tú estás
+él está
+ns estamos
+vs estáis
+ellos están
+/
+bien y tu
+============
+yo estuve
+tú estuviste
+él estuvo
+ns estuvimos
+vs estuvisteis
+ellos estuvieron
+/
+bien ayer
+==================
+yo estaré
+tú estarás
+él estará
+ns estaremos
+vs estaréis
+ellos estarán
+/
+bien mañana
+================
+yo soy
+tú eres
+él es
+ns somos
+vs sois
+ellos son
+
+yo fui
+tú fuiste
+él fue
+ns fuimos
+vs fuisteis
+ellos fueron
+
+yo seré
+tú serás
+él será
+ns seremos
+vs seréis
+ellos serán
+/
+de Chicago
+===============
+
+yo hablo
+tú hablas
+él habla
+ns hablamos
+vs habláis
+ellos hablan
+
+yo hablé
+tú hablaste
+él habló
+ns hablamos
+vs hablasteis
+ellos hablaron
+
+yo hablaré
+tú hablarás
+él hablará
+ns hablaremos
+vs hablaréis
+ellos hablarán
+/
+lengua española
+===========
+yo pido
+tú pides
+él pide
+ns pedimos
+vs pedís
+ellos piden
+
+yo pedí
+tú pediste
+él pidió
+ns pedimos
+vs pedisteis
+ellos pidieron
+
+yo pediré
+tú pedirás
+él pedirá
+ns pediremos
+vs pediréis
+ellos pedirán
+
+===============
+
+yo puedo
+tú puedes
+él puede
+ns podemos
+vs podéis
+ellos pueden
+
+yo pude
+tú pudiste
+él pudo
+ns pudimos
+vs pudisteis
+ellos pudieron
+
+yo podré
+tú podrás
+él podrá
+ns podremos
+vs podréis
+ellos podrán
+
+=============
 
 
 
