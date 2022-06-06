89f4a2fec1430ba45d07bc24af03ed4f6450e23a
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-06 09:24:58 -0400

    Add files via upload

---
+++ english/from_book.js
@@ -0,0 +1,70 @@
+const data_from_book =     `
+I
+we
+he
+/
+tried to
+/
+call
+see
+find
+contact you
+come to
+/
+you
+/
+yesterday
+last week
+==================
+she made him
+/
+study better
+come to school
+jump
+remember her
+==================
+did you
+/
+hear
+see
+/
+what
+/
+I
+she
+/
+just
+/
+did
+said
+found
+=================
+pick up a paper
+get up from a chair
+wake up from a long sleep
+=================
+held back
+/
+laugh
+tears
+=================
+she
+/
+explained how to
+/
+get home
+use the phone
+cook breakfast
+=================
+
+
+call off, cancel
+
+
+sigh
+drum
+arm
+chair arm
+nod, nodded
+`;
+
