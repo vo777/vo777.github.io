01d616401a4abade941a6315933d80073414e8ad
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-06 18:58:37 -0400

    Add files via upload

---
+++ english/data006.js
@@ -0,0 +1,168 @@
+const data006 =     `
+
+
+
+
+==
+I read the red book
+I slept in my room
+What dod you do when you have free time
+I do what I like
+What will you do in the evening
+Once upon a time there lived ...
+Who saw this film?
+Did you see this film?
+I will go to see a movie
+Today I am going to see a movie
+==
+I will go there
+/only after
+/I spend some time with my children
+==
+to spend
+spent
+/
+time
+money
+==
+What will you do this weekend?
+she takes horse-riding lessons
+I want to find time to relax
+How long does it take to get from here to your house?
+it depends
+it depends on ...
+it depends on traffic
+===
+I want 
+/
+him
+her
+them
+us
+everyone
+/to be happy
+===
+I want to see her
+I have things to do
+How many hours do you sleep
+She sleeps 5 hours
+===
+.
+how
+/
+much
+little
+/
+time
+money
+love
+water
+air
+hair
+===
+.
+how/
+many
+few
+/
+dollars
+days
+hours
+people
+birds
+animals
+dogs
+hairs
+things
+===
+small
+little
+/
+girl
+===
+what will you do on Sunday?
+===
+on Sunday I will go to my
+/
+country house
+dacha
+===
+I will
+/
+rest
+have rest
+relax
+go wild
+===
+I had good rest
+===
+Will you have time tomorrow?
+===
+everybody
+somebody
+nobody
+/
+knows
+likes
+loves
+will help
+helped
+/
+you
+===
+I know
+/
+everything
+something
+nothing
+/
+about
+/
+it
+you
+===
+I have nothing to hide
+What do you hide?
+hide and seek
+===
+give me
+do you want
+I want
+/
+something
+/
+to eat
+to drink
+===
+I wanted
+I want
+I will want
+/to love
+===
+He went
+/
+everywhere
+somewhere
+nowhere
+===
+I
+/
+always
+sometimes
+never
+/
+do it
+===
+he
+/
+always
+sometimes
+never
+/
+does it
+===
+
+
+
+`;
+
