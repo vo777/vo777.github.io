d18e2fb52db32b5a5e5e3ae90b67a60c9baf6c74
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-06 19:15:22 -0400

    Add files via upload

---
+++ english/call_example.js
@@ -0,0 +1,81 @@
+const data_call_example =     `
+
+
+==
+I call him
+I do not call him
+/
+every day
+often
+==
+I will call him
+I will not call him
+/
+tomorrow
+in three days
+next Monday
+==
+I called him
+I did not call him
+/
+yesterday
+three days ago
+on Monday
+==
+I want to
+I promise to
+I should
+I am trying to
+I would better
+I would never
+/
+call him
+==
+I wanted to
+I promised to
+I should have
+/
+called him
+/
+...
+but I forgot
+==
+I tried to
+/
+call him
+/
+but ...
+==
+I will try to
+I promise I will
+I promise I will not
+/
+call him
+/
+.
+often
+==
+I am calling him
+/
+.
+tomorrow morning
+==
+I have called him
+/
+.
+5 times
+many times
+==
+I was calling him/
+last night at 7 pm
+==
+I have been 
+/
+calling him
+/
+for 3 hours
+since yesterday
+==
+
+`;
+
