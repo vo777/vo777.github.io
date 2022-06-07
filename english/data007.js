c0eaeb61400095a6092b297b3b668e7e1eadde61
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-07 14:50:13 -0400

    Add files via upload

---
+++ english/data007.js
@@ -0,0 +1,76 @@
+const data007 =     `
+
+buy bought
+sell sold
+pay paid
+make made
+choose chose
+
+try tried
+change changed
+show showed
+play played
+turn turned
+
+shoot shot
+fall fell
+miss missed
+cut cut
+=
+
+I will try to help you
+Where did you buy this shirt?
+Will you pay cash or credit card?
+I didn't play in this film
+I want to change (my clothes)
+I want to choose a new dress
+I want to buy a new car
+I will want to buy a car
+When you go home, try to drive slowly
+When I go home, I will try to drive slowly
+==
+Go!
+Don't go!
+Let's go!
+Let's drink!
+==
+If you were at the exhibition, tell us what was there
+When I have money, I will buy a new shirt like his
+When he travels, he buys new clothes
+Lately I buy my clothes only in army shops
+My character often wears army clothes
+Starring in a movie...
+The movie is based on...
+The movie consists of four parts
+After shooting days I went to...
+Why didn't you use...
+Did you do any stunts?
+We had to redo it
+This is why we refused the safety and the stunt girl
+Because in real life ...
+
+==
+When will this film 
+/
+come to screens?
+be released?
+==
+We 
+/
+shot 
+made
+/
+this movie 
+/
+in one month
+in two months
+==
+important
+not so important
+more important
+not so important
+==
+
+
+`;
+
