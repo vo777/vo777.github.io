42148a3a4d33d8774ec46bd155e1c387a637af0d
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-10 15:28:35 -0400

    Add files via upload

--- english/data010.js
+++ english/data010.js
@@ -1,134 +1,49 @@
-const data008 = `
-in
-/
-the room
-the house
-Moscow
-==
-to
-/
-you
-Moscow
-==
-from
-/
-you
-Moscow
-==
-far from
-/
-you
-Moscow
-==
-at
-/
-school
-home
-==
-over
-/
-the ground
-the house
-==
-between
-/
-you and me
-us
-==
-with
-without
-/
-you
-me
-==
-under ground
-on the table
-I do it for you
-tell me about it
-about 3 minutes
-think about it
-I think about you
-in the middle of
-tea without sugar
-==
-go
-look
-/
-up
-down
-back
-away
-in
-out
-==
-look for
-==
-come
-/
-back
-in
-==
-take
-/
-up
-down
-away
-out
-==
-I watch you
-I will go with you to them
-I made this shirt for me
-I don't want to speak with him about it
-What you tell me will be between us
-be yourself
-she is very far from me
-her mind sometimes is very far from my mind
-he wanted to speak with me about it, but I refused
-I hope you will do it for me
-think about it!
+const data010 = `
 
-
-
-==
-==
-how many people are in the room?
-where did you buy your shirt?
-I bought my shirt in a market in South India
-when did you buy it?
-three years ago
-do you like this style?
-yes, it was a special order
-I paid twenty dollars
-he measured me, and the shirt was ready the next day
-why are wearing it today?
-it's my whim
-why do you wear this shirt?
-do you have a t-shirt under it?
-I'm embarassed
-I liked the multocolor pattern
-he said: "come tomorrow"
-he told me to come back the next day
-do they make dresses?
-they make dresses for European women
-to wear
-what do Indiam women wear?
-what material do Indiam women wear?
-why don't you buy real silk?
-woman - women
-man - men
-human - people - peoples
-mankind, humanity
-come in, enter
-society
-
-
-
-
-
-==
-
-
+Was there something interesting in your life?
+Two days ago I went to another country.
+this time I went to ...
+I came back only today
+what is the difference between ...
+it's a very good question
+they are very open and grateful.
+I learn from them
+it's a big event for them
+if you mean ...
+I support you
+I will give you my support
+Will you tell us what you did last Sunday?
+This artist lived in the nineteenth century
+scenes from the Bible
+did you see any paintings that you didn't see before?
+this is a unique chance to see the full collection of paintings...
+this is the last chance...
+contemporary art
+he invited him to build a special church...
+this is the cathedral where the icon was located
+yes, I like this, but I prefer that...
+it's out of fashion now
+it's in fashion now
+a fashionable writer
+I had a guest from Ireland
+now you understand what he says
+if you speak only English...
+ok, ladies and gentlemen, ...
+what did you do after work?
+I went to a restaurant with my friend(s)
+what food do you like?
+what cuisine do you like?
+they had pizza, but we didn't eat it
+I know one Italian restaurant in Rome
+this sign is for tourists
+the owner cooks for you
+it's a small business
+after you have a drink, do you see devil?
+I don't drink so much
+how many jewelry items do you make per day?
+they take longer than one day
+some items take three or four months
+do you take orders from clients?
+what did you say?
 
 `;
-
-
