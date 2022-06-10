16938cf0085bcee783c571a3b26592bc82bcbe27
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-10 09:00:34 -0400

    Add files via upload

---
+++ english/data009.js
@@ -0,0 +1,68 @@
+const data009 = `
+in touch
+now we will try to speak english
+I will ask you some questions
+do you have questions for him?
+he is an expert in ...
+sleep - slept
+I went to bed
+how did you spend time yesterday?
+I was at a TV talk show
+some people say "don't see that film"
+I kept silence
+I remained silent
+he is wise
+everybody should know it
+what do you think about it?
+many stars wanted to play this role
+once I came to ...
+I was not ready
+I answered the phone
+I felt something strange
+I made my own version
+I am happpy with this version
+I am happy with this
+I am happy with it
+he is happy with himself
+we will do it ourselves
+they do it themselves
+this is the happy end of the story
+it's your role
+fortunately
+unfortunately
+often - seldom
+most often, ...
+what is your story?
+what did you do on Sunday?
+our team won
+our team lost
+a person has to show something without words
+at the same time...
+I don't understand how they guessed
+on Sunday I spent time with my family
+I accompanied her to the lesson
+I took here there...
+everything was so calm and we had a really good time
+I don't know if it is real or not
+that's all
+we never met before
+we were sitting and reading the play...
+before discussion we just read the play
+it is important to read the play before discussions and opinions
+it gives a chance...
+do you dance or sing - both - neither
+sense - nonsense
+
+
+
+=
+myself
+yourself
+himself
+herself
+ourselves
+yourselves
+themselves
+
+`;
+
