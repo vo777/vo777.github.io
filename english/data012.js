0264d1e90f450ae8d2d3b37ac5ba1ed46b7e223b
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-12 09:22:50 -0400

    Add files via upload

--- english/data012.js
+++ english/data012.js
@@ -1,133 +1,55 @@
-const data008 = `
-in
+const data012 = `
+I
+we
 /
-the room
-the house
-Moscow
-==
+travel
+will travel
+want to travel
+will fly
+/
 to
 /
-you
-Moscow
+France
+New York
 ==
-from
+we came to a
+we go to a
 /
-you
-Moscow
+hotel
+island
 ==
-far from
+we stay
+we will stay
+I will stay
 /
-you
-Moscow
+in a hotel
+with friends
 ==
-at
-/
-school
-home
+where will you stay?
+sometimes I travel in my dreams
+what do you see there?
+it is enough
+this is why I don't agree with this statement
+possible - impossible
+he likes movies about zombies
+who are zombies?
+we can not know for sure
+they always ask...
+I need your help
+I need something
+it is very strange
+I have a great idea...
+when is your birthday?
+my birthday is on the tenth of September
+sorry, I din't hear
+do you have same color, smaller size?
+they are of the same age
+we travel on the same flight
+it's all the same
+he is the same guy I saw yesterday
+I don't care
 ==
-over
-/
-the ground
-the house
-==
-between
-/
-you and me
-us
-==
-with
-without
-/
-you
-me
-==
-under ground
-on the table
-I do it for you
-tell me about it
-about 3 minutes
-think about it
-I think about you
-in the middle of
-tea without sugar
-==
-go
-look
-/
-up
-down
-back
-away
-in
-out
-==
-look for
-==
-come
-/
-back
-in
-==
-take
-/
-up
-down
-away
-out
-==
-I watch you
-I will go with you to them
-I made this shirt for me
-I don't want to speak with him about it
-What you tell me will be between us
-be yourself
-she is very far from me
-her mind sometimes is very far from my mind
-he wanted to speak with me about it, but I refused
-I hope you will do it for me
-think about it!
-
-
-
-==
-==
-how many people are in the room?
-where did you buy your shirt?
-I bought my shirt in a market in South India
-when did you buy it?
-three years ago
-do you like this style?
-yes, it was a special order
-I paid twenty dollars
-he measured me, and the shirt was ready the next day
-why are wearing it today?
-it's my whim
-why do you wear this shirt?
-do you have a t-shirt under it?
-I'm embarassed
-I liked the multocolor pattern
-he said: "come tomorrow"
-he told me to come back the next day
-do they make dresses?
-they make dresses for European women
-to wear
-what do Indiam women wear?
-what material do Indiam women wear?
-why don't you buy real silk?
-woman - women
-man - men
-human - people - peoples
-mankind, humanity
-come in, enter
-society
-
-
-
-
-
-==
-
-
+first, second, third, fourth, fifth
 
 `;
 
