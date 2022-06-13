222aa5af1fa3449c08036dfe9cc19ff9da34f7dd
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-12 20:33:46 -0400

    Add files via upload

--- german/examples.js
+++ german/examples.js
@@ -1,5 +1,146 @@
 // German examples:
 const examples = `
 Noch keine Beispiele
+Hallo
+Hallo, wie geht’s?
+Guten Morgen
+Guten Tag
+
+Guten Abend
+Gute Nacht
+Vielen Dank
+Ich danke Ihnen auch
+Tschüss, bis zum nächsten Mal
+Schönes Wetter heute, nicht wahr?
+Mein Name ist _
+Ich bin Amerikaner, Kanadier, Engländer
+Ich bin Amerikanerin, Kanadierin, Engländerin
+I'm American ( Canadian ( English
+Woher kommen Sie?
+Freut mich
+Es tut mir leid, aber ich verstehe nicht
+Ich spreche nicht gut Deutsch
+Können Sie das bitte wiederholen?
+Können Sie bitte langsamer sprechen?
+Schreiben Sie das bitte für mich auf
+Was bedeutet das?
+Sprechen Sie Englisch?
+Es tut mir leid
+Ich weiß nicht
+In Ordnung
+Macht nichts
+null
+eins
+zwei
+drei
+vier
+fünf
+sechs
+sieben
+acht
+neun
+zehn
+elf
+zwölf
+dreizehn
+vierzehn
+fünfzehn
+sechzehn
+siebzehn
+achtzehn
+neunzehn
+zwanzig 
+einundzwanzig
+zweiundzwanzig
+dreiundzwanzig 
+vierundzwanzig
+fünfundzwanzig
+sechsundzwanzig
+siebenundzwanzig 
+achtundzwanzig
+neunundzwanzig
+dreißig
+einunddreißig
+zweiunddreißig
+vierzig
+fünfzig
+sechzig
+siebzig
+achtzig
+neunzig
+hundert
+zweihundertfünfzig
+fünfhundert
+siebenhundertdreiundachtzig
+tausend
+
+Ein Tisch für eine Person bitte
+Ein Tisch für zwei Personen, bitte
+Haben Sie schon auf?
+Können wir (auf einen Tisch) warten?
+Können wir dort sitzen?
+Entschuldigung!
+Was empfehlen Sie?
+Was ist das beliebteste Gericht?
+Was ist das?
+Was für Bier haben Sie?
+Ein kleines Bier bitte
+Ein großes Bier bitte
+Bringen Sie mir bitte eine Auswahl von leckeren Sachen
+Bitte wählen Sie etwas
+Die Rechnung, bitte
+Kann ich bitte die Speisekarte haben?
+Ich möchte nach _____
+Wann fährt der nächste Zug ( Bus nach _____?
+Was kostet das?
+Einmal( zweimal (nach _____), bitte
+Wie lange dauert das?
+Wohin muss ich jetzt gehen?
+Wann fährt er ab?
+
+Wie spät ist es (jetzt)?
+Hält der Zug( Bus in _?
+Entschuldigen Sie, ist dies _____?
+Können Sie das bitte für mich aufschreiben?
+Zeigen Sie mir das bitte auf der Karte?
+Wo ist _____ auf der Karte?
+Entschuldigung, darf ich Sie etwas fragen?
+Ich möchte nach _____
+Ich möchte dahin
+Ich habe mich verlaufen
+Ich habe mich verfahren
+Wie komme ich dahin?
+Geht es hier lang?
+Zeigen Sie mir das bitte auf der Karte?
+Wo ist __?
+Das gefällt mir
+Was kostet das?
+Bitte wiederholen Sie das
+Schreiben Sie das bitte für mich auf?
+Und wenn ich das alles kaufe?
+Das ist mir zu teuer
+Geben Sie mir einen Rabatt?
+Ich suche nach _____
+Ich schaue mich nur um
+Danke, ich suche noch weiter
+Moment, bitte
+Ja, bitte
+Nein, danke
+Können Sie mir bitte helfen?
+Ich brauche einen Arzt
+Es geht mir nicht gut
+Es geht ihm(ihr nicht gut
+Gibt es ein Krankenhaus in der Nähe?
+Fahren Sie mich bitte zum Krankenhaus
+Es tut hier weh
+Ich brauche Medizin
+Es tut mir leid, Sie zu stören, aber…
+Kann ich Sie schnell etwas fragen?
+Ich suche ein Restaurant mit gutem Essen hier in der Nähe
+Ich suche ein nettes Café in der Nähe
+Wissen Sie etwas über _____?
+Gibt es hier in der Nähe etwas Interessantes zu sehen?
+Trotzdem danke
+
 `
 ;
