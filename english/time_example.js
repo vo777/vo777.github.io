0bff1a4c4a1b686c1c521c23dd7fee9d299c815f
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-14 10:24:45 -0400

    Add files via upload

---
+++ english/time_example.js
@@ -0,0 +1,30 @@
+const data_time_example =     `
+
+I swam yesterday
+I will swim tomorrow
+I swim every day
+/
+.
+morning
+before breakfast
+after breakfast
+between 10 and 11am
+since 10 till 11am
+until dark
+for 2 hours
+==
+I have been swimming
+/
+since 10am
+for 2 hours
+==
+I was swimming yesterday
+I will be swimming tomorrow
+/
+at 7pm
+==
+I am swimming
+==
+
+`;
+
