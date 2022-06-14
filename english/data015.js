66c993818a0ff4c10614bf11f334648b1b87e7ad
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-14 09:12:19 -0400

    Add files via upload

--- english/data015.js
+++ english/data015.js
@@ -1,4 +1,138 @@
 const data015 = `
+tell me about 
+/
+yourself
+yourselves
+==
+when she grows up, ...
+==
+in what sense?
+nonsense
+==
+what kind of profession does your husband have?
+==
+very interesting
+==
+thank you. very nice story
+=
+where does he want to work?
+what profession does he like to have?
+=
+tastes differ
+=
+so what?
+=
+will he want to 
+/
+go?
+be?
+==
+does he like ... ?
+==
+my friends call me ... 
+==
+when I was younger, I liked classical music
+=
+do you make your own music?
+when I do it at home, she does not like it
+=
+it looks like ...
+it sound like ...
+=
+I'm married to ...
+=
+what I do, I do for myself
+=
+when? - right now
+=
+to be
+/
+right
+wrong
+=
+we'll see
+we make no plans
+=
+my children like to travel
+I want them to know English
+=
+the elder
+the younger
+/
+likes ...
+=
+he designes clothes for ...
+=
+with all that, ...
+=
+he always praises him ...
+=
+are they friends?
+=
+in some families children are not friends...
+=
+when I scold one of them, the other comes to me and defends him.
+=
+do you want to tell us about yourself?
+tell us your life story
+=
+what do you like to drink?
+=
+I work very much
+=
+I like to travel, but it requires time
+=
+I felt refreshed
+=
+it's an ideal sport if you have little time
+=
+it was a joke
+=
+I have a dog
+=
+my brother called me this name...
+=
+I have
+/
+big
+tremendous
+/
+plans
+=
+short-term 
+long-term
+/
+plans
+==
+when you are great and famous, please don't forget us
+=
+it takes all your time to ...
+=
+a very rare profession
+=
+how
+when
+/
+did you decide to become ...
+=
+where did you learn ...
+=
+what sport did you practice?
+=
+what about you?
+what is your present situation and your plans for the future?
+=
+later you can change something
+=
+what art styles do you like?
+what art do you like?
+=
+I know to much about it
+I have a different destiny
+=
+
+
+
 
 
 `;
