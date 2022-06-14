94d26e82f526c00a6c4eb9415c308ca357e6ff25
vo777 <76571140+vo777@users.noreply.github.com>
2022-06-14 11:21:16 -0400

    Add files via upload

--- english/data016.js
+++ english/data016.js
@@ -1,6 +1,55 @@
 const data016 = `
-
+remember
+for
+with
+without
+about
+/
+each other
+==
+he
+/
+loves
+is loved
+sees
+is seen
+goes
+is gone
+==
+the house
+/
+is being built
+was built in ... year
+will be built
+==
+it
+/
+was done
+will be done
+==
+this question was
+/
+asked
+discussed
+answered
+==
+I have been with you
+we have been together
+/
+for 5 years
+==
+I have neve been in London
+when was the gallery founded?
+==
+my first book was published 10 years ago
+the script was written last year
+this book was writtel
+this role was played
+this house was sold
+this car was bought
+/
+by Alex
+==
 
 `;
-
 
