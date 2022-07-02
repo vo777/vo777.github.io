274869962ac65e1d8ac1e26ecbd1a989e4d72b81
vo777 <76571140+vo777@users.noreply.github.com>
2022-07-02 16:27:09 -0400

    Add files via upload

--- french/examples.js
+++ french/examples.js
@@ -1,5 +1,77 @@
 // French examples:
 const examples = `
-non non non non non
+
+J'aime être ici.
+Je veux être ici.
+je dois être ici
+Je peux être ici.
+J'essaie d'être ici.
+Aimez-vous être ici?
+Voulez-vous être ici?
+Vous devez être ici ?
+peux-tu être ici
+Avez-vous l'intention d'être ici?
+J'aime être étudiant.
+Je veux être étudiant
+je dois être étudiant
+Je peux être étudiant.
+J'essaie d'être étudiant.
+Aimez-vous être étudiant?
+Voulez-vous être étudiant?
+Faut-il être étudiant ?
+Pouvez-vous être étudiant?
+Essayez-vous d'être étudiant?
+J'aime avoir une voiture.
+je veux avoir une voiture
+Je dois avoir une voiture.
+Je peux avoir une voiture.
+J'essaie d'avoir une voiture.
+Aimez-vous avoir une voiture?
+Voulez-vous avoir une voiture?
+Faut-il avoir une voiture ?
+pouvez-vous avoir une voiture
+Vous essayez d'avoir une voiture ?
+J'aime aller en Floride.
+Je veux aller en Floride.
+Je dois aller en Floride.
+Je peux aller en Floride.
+J'essaie d'aller en Floride.
+Aimez-vous aller en Floride ?
+Voulez-vous aller en Floride ?
+Vous devez vous rendre en Floride ?
+Pouvez-vous aller en Floride ?
+Vous essayez d'aller en Floride ?
+J'aime faire les devoirs.
+Je veux faire les devoirs.
+Je dois faire les devoirs.
+Je peux faire les devoirs.
+J'essaie de faire mes devoirs.
+Aimez-vous faire vos devoirs?
+Voulez-vous faire des devoirs?
+Tu dois faire les devoirs ?
+Pouvez-vous faire les devoirs?
+Essayez-vous de faire vos devoirs?
+J'aime regarder les nouvelles.
+Je veux voir les nouvelles.
+Je dois regarder les nouvelles.
+Je peux regarder les nouvelles.
+J'essaie de regarder les nouvelles.
+Aimez-vous regarder les nouvelles?
+Voulez-vous voir les nouvelles?
+Faut-il regarder les actualités ?
+Pouvez-vous regarder les nouvelles?
+Essayez-vous de regarder les nouvelles?
+
+j'étais en Floride
+je suis en Floride
+Je serai en Floride.
+Je viens de Floride.
+
+J'ai vécu en Floride.
+Je vis en Floride.
+Je vais vivre en Floride.
+====
+
+
 `
 ;
