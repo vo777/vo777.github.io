b894eb5344c6ca4a8803251ffa05191ef803d28c
vo777 <76571140+vo777@users.noreply.github.com>
2022-07-02 16:28:31 -0400

    Add files via upload

--- portuguese/examples.js
+++ portuguese/examples.js
@@ -1,5 +1,80 @@
 // Portuguese examples:
 const examples = `
-0 0 0 0 0
+
+Eu gosto de estar aqui.
+Eu quero estar aqui.
+eu tenho que estar aqui
+Eu posso estar aqui.
+Eu tento estar aqui.
+Você gosta de estar aqui?
+Você quer estar aqui?
+Você tem que estar aqui?
+você pode estar aqui?
+Você pretende estar aqui?
+Eu gosto de ser estudante.
+Eu quero ser um estudante
+eu tenho que ser um estudante
+Eu posso ser um estudante.
+Eu tento ser um estudante.
+Você gosta de ser estudante?
+Você quer ser um estudante?
+Você tem que ser um estudante?
+Você pode ser um estudante?
+Você está tentando ser um estudante?
+Eu gosto de ter um carro.
+eu quero ter um carro
+Eu tenho que ter um carro.
+Eu posso ter um carro.
+Eu tento ter um carro.
+Você gosta de ter um carro?
+Você quer ter um carro?
+Você tem que ter um carro?
+você pode ter um carro?
+Você está tentando ter um carro?
+Eu gosto de ir para a Flórida.
+Eu quero ir para a Flórida.
+Eu tenho que ir para a Flórida.
+Eu posso ir para a Flórida.
+Estou tentando ir para a Flórida.
+Você gosta de ir para a Flórida?
+Você quer ir para a Flórida?
+Você tem que ir para a Flórida?
+Você pode ir para a Flórida?
+Você está tentando ir para a Flórida?
+Eu gosto de fazer lição de casa.
+Eu quero fazer a lição de casa.
+Tenho que fazer a tarefa.
+Eu posso fazer a lição de casa.
+Eu tento fazer a lição de casa.
+Você gosta de fazer lição de casa?
+Você quer fazer lição de casa?
+Você tem que fazer a lição de casa?
+Você pode fazer a lição de casa?
+Você tenta fazer a lição de casa?
+Eu gosto de ver as notícias.
+Eu quero ver as novidades.
+Tenho que ver as notícias.
+Eu posso ver as notícias.
+Eu tento assistir as notícias.
+Você gosta de ver as notícias?
+Quer ver as novidades?
+Você tem que assistir as notícias?
+Você pode assistir as notícias?
+Você está tentando assistir as notícias?
+
+eu estava na Flórida
+eu estou na Flórida
+Estarei na Flórida.
+Eu sou da Flórida.
+
+Eu morava na Flórida.
+Eu vivo na Florida.
+Vou morar na Flórida.
+
+=
+
+
+
+
 `
 ;
